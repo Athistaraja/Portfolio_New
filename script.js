8b70f085da090cc3941c80a3268827e4349ad580
Athistaraja <athisraja45@gmail.com>
2024-12-17 23:04:21 +0530

    contact_form_update

--- script.js
+++ script.js
@@ -11,26 +11,5 @@ function toggleMenu() {
   const navMenu = document.querySelector('nav ul');
   navMenu.classList.toggle('active');
 }
-  
-const highlightText = document.getElementById("highlight");
 
-// Array of titles to cycle through
-const titles = [ "Backend Developer", "Full Stack Developer", "UI/UX", "Web"];
-let index = 0;
 
-// Function to change the highlight text
-function changeHighlight() {
-  // Update the text content
-  highlightText.textContent = titles[index];
-
-  // Restart animations
-  highlightText.classList.remove("highlight");
-  void highlightText.offsetWidth; // Trigger reflow to restart animation
-  highlightText.classList.add("highlight");
-
-  // Move to the next title
-  index = (index + 1) % titles.length; // Loop back to the first item
-}
-
-// Change text every 10 seconds
-setInterval(changeHighlight, 5000);
